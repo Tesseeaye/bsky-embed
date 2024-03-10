ebd3211dc69d507dd70a37c274237ed1d9683f13
Tesseeaye <terryfjunejr@pm.me>
2024-03-10 19:50:56 -0400

    fixed

--- src/BskyEmbed.tsx
+++ src/BskyEmbed.tsx
@@ -70,9 +70,9 @@ const BskyEmbed: Component<Props> = ({
       <style>
         {styles}
       </style>
-      <section class={`${mode} max-w-screen-sm mx-auto space-y-6`}>
+      <section class={`${mode} max-w-screen-sm mx-auto space-y-4`}>
         {isLoading() && Array.from(Array(limit)).map(() =>
-          <article class="flex gap-2 px-10 py-4 bg-zinc-50 rounded-lg drop-shadow-sm border border-gray-200 animate-pulse">
+          <article class="flex gap-2 px-10 py-6 bg-zinc-50 rounded-lg drop-shadow-sm border border-gray-200 animate-pulse">
             <div class="bg-slate-300 w-12 h-12 rounded-full dark:bg-slate-800"></div>
             <div class="flex-1 space-y-2 py-1">
               <div class="grid grid-cols-4 gap-4">
@@ -84,7 +84,7 @@ const BskyEmbed: Component<Props> = ({
             </div>
           </article>
         )}
-        {!isLoading() && feedData().map(post => <article class="px-10 py-4 mx-auto bg-zinc-50 rounded-lg drop-shadow-sm border border-gray-200">
+        {!isLoading() && feedData().map(post => <article class="px-10 py-6 mx-auto bg-zinc-50 rounded-lg drop-shadow-sm border border-gray-200">
           { post.isRepost && <p class="flex gap-1 items-center ml-10 text-slate-600 dark:text-slate-400">
             <svg viewBox="0 0 576 512" height="16" width="16" tabindex="-1" class="mr-1"><path fill="currentColor" d="M272 416c17.7 0 32-14.3 32-32s-14.3-32-32-32H160c-17.7 0-32-14.3-32-32V192h32c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l32 0 0 128c0 53 43 96 96 96H272zM304 96c-17.7 0-32 14.3-32 32s14.3 32 32 32l112 0c17.7 0 32 14.3 32 32l0 128H416c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8l-32 0V192c0-53-43-96-96-96L304 96z"></path></svg>
             <span class="text-sm text-slate-500 font-semibold">Reposted by {post.repostBy}</span>
